c4dbe2ae852d46135597e6d4233cbb6febf725ac
siddharth51292 <siddharth.avadhanam@gmail.com>
2016-02-01 23:18:46 -0500

    1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a list of functions and stores the matrix input 
+##the second one computes
+## its inverse after first checking if is already available. 
 
-## Write a short comment describing this function
+## This function takes a matrix argument and returns a list of 
+## functions which can either retreive the matrix, superassign 
+## the variable inv, retreive the inverse, or set a new matrix to x
+## using superassignment. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+inv <- NULL
+set <- function(y){
+  x <<- y
+  inv <<- NULL
+  
+}
+get <- function() x 
+setinv <- function(inverse) inv <<- inverse
+getinv <- function() inv 
+list(set=set, get=get, setinv = setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function inverts its matrix argument 
+## only if there is nothing stored in the variable inv. 
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("retreiving cached data...")
+    return(inv)
+    }
+  matrix <- x$get()
+  inv <- solve(matrix)
+  x$setinv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
